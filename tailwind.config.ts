e6e39994c8aad7344ce1d3545c27db10a3c63115
myungsangBaek <bms6168@naver.com>
2025-01-27 11:41:02 +0900

    feat: 프로젝트 초기 세팅 + 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      animation: {
+        'fade-in': 'fadeInUp 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'scale(0.8)' },
+          '50%': { opacity: '0.5', transform: 'scale(1)' },
+          '100%': { opacity: '1' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px) scale(1)' },
+          '100%': { opacity: '1', transform: 'translateY(0px) scale(1)' },
+        },
       },
     },
   },
